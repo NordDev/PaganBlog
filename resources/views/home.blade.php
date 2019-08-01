542c3242e801d338cdd5648fa0987423a966b48b
North-Guard <sver4ok@protonmail.com>
2019-08-01 13:58:02 +0300

    upload all files for my project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>PaganBlog</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Блог о язычестве">
+	<meta name="keywords" content="pagan, язычество, боги	">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="header-warp">
+			<div class="header-social d-flex justify-content-end">
+				<a href="#"><i class="fab fa-pinterest"></i></a>
+				<a href="#"><i class="fab fa-facebook"></i></a>
+				<a href="#"><i class="fab fa-twitter"></i></a>
+				<a href="#"><i class="fab fa-dribbble"></i></a>
+				<a href="#"><i class="fab fa-behance"></i></a>
+			</div>
+			<div class="header-bar-warp d-flex">
+				<!-- site logo -->
+				<a href="home.blade.php" class="navbar-left">
+					<img src="./img/logo.png" alt="">
+				</a>
+				<nav class="top-nav-area w-100">
+					<div class="user-panel">
+						<a href="">Логин</a> / <a href="">Регистрация</a>
+					</div>
+					<!-- Menu -->
+					<ul class="main-menu primary-menu">
+						<li><a href="home.blade.php">Главная</a></li>
+						<li><a href="blog.html">Блог</a></li>
+						<li><a href="games.html">Боги</a>
+							<ul class="sub-menu">
+								<li><a href="game-single.html">Game Singel</a></li>
+							</ul>
+						</li>
+						<li><a href="review.html">Обзоры</a></li>
+						<li><a href="contact.html">Контакты</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<!-- Header section end -->
+	<section class="header-forest set-bg" data-setbg="img/slider-bg-3.jpg">
+	</section>
+
+	<!-- Blog section -->
+	<section class="blog-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-9 col-lg-8 col-md-7">
+					<div class="section-title text-white">
+						<h2 align="center">Последние статьи</h2>
+					</div>
+
+					<!-- Blog item -->
+					<div class="blog-item">
+						<div class="blog-thumb">
+							<img src="./img/blog/1.jpg" alt="">
+						</div>
+						<div class="blog-text text-box text-white">
+							<div class="top-meta">11.11.18  /  in <a href="">Категория</a></div>
+							<h3>Название крутой статьи!</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius-mod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consecte-tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....</p>
+							<a href="#" class="read-more">Читать...  <img src="img/icons/double-arrow.png" alt="#"/></a>
+						</div>
+					</div>
+					<!-- Blog item -->
+					<div class="blog-item">
+						<div class="blog-thumb">
+							<img src="./img/blog/1.jpg" alt="">
+						</div>
+						<div class="blog-text text-box text-white">
+							<div class="top-meta">11.11.18  /  in <a href="">Категория</a></div>
+							<h3>Название крутой статьи!</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius-mod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consecte-tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....</p>
+							<a href="#" class="read-more">Читать...  <img src="img/icons/double-arrow.png" alt="#"/></a>
+						</div>
+					</div>
+					<!-- Blog item -->
+					<div class="blog-item">
+						<div class="blog-thumb">
+							<img src="./img/blog/1.jpg" alt="">
+						</div>
+						<div class="blog-text text-box text-white">
+							<div class="top-meta">11.11.18  /  in <a href="">Категория</a></div>
+							<h3>Название крутой статьи!</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius-mod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consecte-tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....</p>
+							<a href="#" class="read-more">Читать...  <img src="img/icons/double-arrow.png" alt="#"/></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-4 col-md-5 sidebar">
+					<div id="stickySidebar">
+						<div class="widget-item">
+							<h4 class="widget-title">Лучшее</h4>
+							<div class="trending-widget">
+								<div class="tw-item">
+									<div class="tw-thumb">
+										<img src="./img/blog-widget/1.jpg" alt="#">
+									</div>
+									<div class="tw-text">
+										<div class="tw-meta">11.11.18  /  in <a href="">Категория</a></div>
+										<h5>Название крутой статьи!</h5>
+									</div>
+								</div>
+								<div class="tw-item">
+									<div class="tw-thumb">
+										<img src="./img/blog-widget/1.jpg" alt="#">
+									</div>
+									<div class="tw-text">
+										<div class="tw-meta">11.11.18  /  in <a href="">Категория</a></div>
+										<h5>Название крутой статьи!</h5>
+									</div>
+								</div>
+								<div class="tw-item">
+									<div class="tw-thumb">
+										<img src="./img/blog-widget/1.jpg" alt="#">
+									</div>
+									<div class="tw-text">
+										<div class="tw-meta">11.11.18  /  in <a href="">Категория</a></div>
+										<h5>Название крутой статьи!</h5>
+									</div>
+								</div>
+								<div class="tw-item">
+									<div class="tw-thumb">
+										<img src="./img/blog-widget/1.jpg" alt="#">
+									</div>
+									<div class="tw-text">
+										<div class="tw-meta">11.11.18  /  in <a href="">Категория</a></div>
+										<h5>Название крутой статьи!</h5>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Blog section end -->
+
+
+	<!-- Newsletter section -->
+	<section class="newsletter-section">
+		<div class="container">
+			<h2>Подписка на новости</h2>
+			<form class="newsletter-form">
+				<input type="text" placeholder="Введите свою почту">
+				<button class="site-btn">Подписаться!  <img src="img/icons/double-arrow.png" alt="#"/></button>
+			</form>
+		</div>
+	</section>
+	<!-- Newsletter section end -->
+
+
+	<!-- Footer section -->
+	<footer class="footer-section">
+		<div class="container">
+			<div class="footer-left-pic">
+				<img src="img/footer-left-pic.png" alt="">
+			</div>
+			<div class="footer-right-pic">
+				<img src="img/footer-right-pic.png" alt="">
+			</div>
+			<a href="#" class="footer-logo">
+				<img src="./img/logo.png" alt="">
+			</a>
+			<ul class="main-menu footer-menu">
+				<li><a href="">Главная</a></li>
+				<li><a href="">Блог</a></li>
+				<li><a href="">Контакты</a></li>
+			</ul>
+			<div class="footer-social d-flex justify-content-center">
+				<a href="#"><i class="fab fa-pinterest"></i></a>
+				<a href="#"><i class="fab fa-facebook"></i></a>
+				<a href="#"><i class="fab fa-twitter"></i></a>
+				<a href="#"><i class="fab fa-dribbble"></i></a>
+				<a href="#"><i class="fab fa-behance"></i></a>
+			</div>
+			<div class="copyright"><a href="">North_Guard</a> 	2019</div>
+		</div>
+	</footer>
+	<!-- Footer section end -->
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.sticky-sidebar.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/FontAwesome.js"></script>
+
+	</body>
+</html>
